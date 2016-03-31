5ead8da7e049f0db51679a51ed8a4ae540e54aa7
Namjae Jeon <namjae.jeon@protocolfreedom.org>
2016-03-31 14:31:39 +0900

    cifssrv: fix missing check in smb 3.11 dialet
    Signed-off-by: Namjae Jeon <namjae.jeon@protocolfreedom.org>

--- srv.c
+++ srv.c
@@ -462,7 +462,8 @@ again:
 				server->dialect == SMB21_PROT_ID ||
 				server->dialect == SMB2X_PROT_ID ||
 				server->dialect == SMB30_PROT_ID ||
-				server->dialect == SMB302_PROT_ID)) {
+				server->dialect == SMB302_PROT_ID ||
+				server->dialect == SMB311_PROT_ID)) {
 		cifssrv_debug("Need to send the smb2 negotiate response\n");
 		init_smb2_neg_rsp(smb_work);
 		goto send;
