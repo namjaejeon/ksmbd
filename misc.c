6afac2bf420ab45c300f38efeb53680884f3930c
Vivek Trivedi <t.vivek@samsung.com>
2017-01-09 14:19:35 +0900

    cifssrv: enable SMB311 dialect
    To enforce signing at server side, add below global config in
    /etc/cifs/smb.conf
    
    [global]
            server signing = mandatory
    
    Signed-off-by: Vivek Trivedi <t.vivek@samsung.com>
    Signed-off-by: Namjae Jeon <namjae.jeon@protocolfreedom.org>

--- misc.c
+++ misc.c
@@ -43,8 +43,7 @@ static struct {
 	{SMB2X_PROT, "\2SMB 2.???", "SMB2_22", SMB2X_PROT_ID},
 	{SMB30_PROT, "\2SMB 3.0", "SMB3_00", SMB30_PROT_ID},
 	{SMB302_PROT, "\2SMB 3.02", "SMB3_02", SMB302_PROT_ID},
-	/* temporary 3.1.1 dialect disable */
-//	{SMB311_PROT, "\2SMB 3.1.1", "SMB3_11", SMB311_PROT_ID},
+	{SMB311_PROT, "\2SMB 3.1.1", "SMB3_11", SMB311_PROT_ID},
 #endif
 };
 
@@ -399,7 +398,7 @@ int negotiate_dialect(void *buf)
 	int start_index, ret = BAD_PROT_ID;
 
 #ifdef CONFIG_CIFS_SMB2_SERVER
-	start_index = SMB302_PROT;
+	start_index = SMB311_PROT;
 #else
 	start_index = CIFS_PROT;
 #endif
