c1e8e6a0eccd72a5127397a3b8708eb2dec37418
Taeyang Mok <t.mok@samsung.com>
2017-09-04 16:32:44 +0900

    cifsd: fix some smb2.setinfo failures
    1. make setinfo not to allow to change non-directory file's attributes
    into ATTR_DIRECTORY.
    
    2. implement set_info FILE_POSITION_INFORMATION case.
    
    Signed-off-by: Taeyang Mok <t.mok@samsung.com>

--- vfs.c
+++ vfs.c
@@ -29,6 +29,8 @@
 #include <linux/xattr.h>
 #endif
 #include <linux/falloc.h>
+#include <linux/genhd.h>
+#include <linux/blkdev.h>
 
 #include "export.h"
 #include "glob.h"
@@ -1246,4 +1248,24 @@ int smb_vfs_alloc_size(struct file *filp, loff_t len)
 int smb_vfs_remove_xattr(struct file *filp, char *field_name)
 {
 	return vfs_removexattr(filp->f_path.dentry, field_name);
+}
+
+/*
+ * get_logical_sector_size() - get logical sector size from inode
+ * @inode: inode
+ *
+ * Return: logical sector size
+ */
+unsigned short get_logical_sector_size(struct inode *inode)
+{
+	struct request_queue *q;
+	int ret_val;
+
+	ret_val = 512;
+	q = inode->i_sb->s_bdev->bd_disk->queue;
+
+	if (q && q->limits.logical_block_size)
+		ret_val = q->limits.logical_block_size;
+
+	return ret_val;
 }
