e62d4fda3730fe65ab5a72224f1b4eb7bc8676cb
Yunjae Lim <yunjae.lim@samsung.com>
2018-05-15 14:16:21 +0900

    cifsd: fix wrong free call to conn->preauth_info
    This is a bugfix for commit id 0481530, "use kmalloc instead of ...".
    
    Even though it goes out to the exception in smb2_negotiate, the
    preauth_info is still used for error response.
    
    Unable to handle kernel NULL pointer dereference at virtual address 000000
    Backtrace:
    (calc_preauth_integrity_hash) from [<c0286700>] (smb3_preauth_hash_rsp+0x9
    (smb3_preauth_hash_rsp) from [<c02791ac>] (handle_smb_work+0x4e4/0x584)
    (handle_smb_work) from [<c00490f4>] (process_one_work+0x200/0x508)
    
    Signed-off-by: Yunjae Lim <yunjae.lim@samsung.com>

--- smb2pdu.c
+++ smb2pdu.c
@@ -1132,11 +1132,8 @@ int smb2_negotiate(struct smb_work *smb_work)
 	cifsd_tcp_set_need_negotiate(smb_work);
 
 err_out:
-	if (rc < 0) {
+	if (rc < 0)
 		smb2_set_err_rsp(smb_work);
-		kfree(conn->preauth_info);
-		conn->preauth_info = NULL;
-	}
 
 	return rc;
 }
