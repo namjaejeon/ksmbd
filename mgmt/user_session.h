19cfe45307eb31d528559f9aa2f4eed1732a93a4
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2018-08-30 11:21:03 +0900

    cifsd: remove cifsd_ses_global_list from user_session
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- mgmt/user_session.h
+++ mgmt/user_session.h
@@ -74,9 +74,7 @@ struct cifsd_session {
 	__u8				smb3decryptionkey[SMB3_SIGN_KEY_SIZE];
 	__u8				smb3signingkey[SMB3_SIGN_KEY_SIZE];
 
-	/* REMOVE */
 	struct list_head		cifsd_ses_list;
-	struct list_head		cifsd_ses_global_list;
 };
 
 static inline int test_session_flag(struct cifsd_session *sess, int bit)
