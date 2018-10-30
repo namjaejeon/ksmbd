40f7154ac27741fb7d6b2857ca9b4c74b9aa65ea
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2018-10-30 14:38:15 +0900

    cifsd: add Copyright (C) 2018 Namjae Jeon
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- smb_common.c
+++ smb_common.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
  *   Copyright (C) 2018 Samsung Electronics Co., Ltd.
+ *   Copyright (C) 2018 Namjae Jeon <namjae.jeon@protocolfreedom.org>
  */
 
 #include "smb_common.h"
