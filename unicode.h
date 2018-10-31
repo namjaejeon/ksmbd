52c24a354bb6159fe1d0c2734163eaf66ef539c6
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2018-10-31 16:45:12 +0900

    cifsd: cifsd_strndup_to_utf16 and cifs_local_to_utf16_bytes are not used
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- unicode.h
+++ unicode.h
@@ -74,11 +74,6 @@ char *smb_strndup_from_utf16(const char *src, const int maxlen,
 extern int smbConvertToUTF16(__le16 *target, const char *source, int maxlen,
 		const struct nls_table *cp, int mapChars);
 extern char *extract_sharename(char *treename);
-#ifdef CONFIG_CIFS_SMB2
-extern __le16 *cifsd_strndup_to_utf16(const char *src, const int maxlen,
-		int *utf16_len, const struct nls_table *cp,
-		int remap);
-#endif /* CONFIG_CIFS_SMB2 */
 #endif
 
 wchar_t cifs_toupper(wchar_t in);
