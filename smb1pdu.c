e02aeb21c68863283b33219aec78dc77f587116d
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2018-11-20 18:16:14 +0900

    cifsd: do not leak d_info.name in find_first()
    We can leak d_info.name within the find_first() loop in many places.
    Basically the only branch that takes care of ->name pointer is
    smb_populate_readdir_entry() error path.
    
    Move ->name deallocation to the start fo the loop, just like in
    smb2 case.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- smb1pdu.c
+++ smb1pdu.c
@@ -5844,7 +5844,11 @@ static int find_first(struct cifsd_work *work)
 			goto err_out;
 	}
 
+	d_info.name = NULL;
 	do {
+		kfree(d_info.name);
+		d_info.name = NULL;
+
 		if (dir_fp->dirent_offset >= dir_fp->readdir_data.used) {
 			dir_fp->dirent_offset = 0;
 			r_data.used = 0;
@@ -5882,39 +5886,38 @@ static int find_first(struct cifsd_work *work)
 			continue;
 
 		cifsd_kstat.kstat = &kstat;
-		d_info.name = cifsd_vfs_readdir_name(work, &cifsd_kstat, de,
-			dirpath);
+		d_info.name = cifsd_vfs_readdir_name(work,
+						     &cifsd_kstat,
+						     de,
+						     dirpath);
 		if (IS_ERR(d_info.name)) {
-			rc = PTR_ERR(d_info.name);
-			cifsd_debug("Err while dirent read rc = %d\n", rc);
-			rc = 0;
+			cifsd_debug("Cannot read dirent: %d\n",
+				    (int)PTR_ERR(d_info.name));
 			continue;
 		}
 
 		if (!strncmp(de->name, ".", de->namelen) ||
-			!strncmp(de->name, "..", de->namelen)) {
+			!strncmp(de->name, "..", de->namelen))
 			continue;
-		}
 
 		if (cifsd_share_veto_filename(share, d_info.name)) {
-			cifsd_debug("file(%s) is invisible by setting as veto file\n",
-				d_info.name);
+			cifsd_debug("Veto filename %s\n", d_info.name);
 			continue;
 		}
 
 		if (is_matched(d_info.name, srch_ptr)) {
 			rc = smb_populate_readdir_entry(conn,
-				req_params->InformationLevel, &d_info,
-				&cifsd_kstat);
+						req_params->InformationLevel,
+						&d_info,
+						&cifsd_kstat);
 			if (rc) {
 				kfree(d_info.name);
 				goto err_out;
 			}
 		}
-
-		kfree(d_info.name);
 	} while (d_info.out_buf_len >= 0);
 
+	kfree(d_info.name);
 	if (!d_info.data_count && *srch_ptr) {
 		cifsd_debug("There is no entry matched with the search pattern\n");
 		rsp->hdr.Status.CifsError = NT_STATUS_NO_SUCH_FILE;
