ab321532a2bcbae50573c44273becb3d6a0ac5f1
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2018-12-20 15:36:48 +0900

    cifsd: reorder smb_version_values to avoid padding
    add/remove: 0/0 grow/shrink: 0/6 up/down: 0/-48 (-48)
    Function                                     old     new   delta
    smb311_server_values                         120     112      -8
    smb30_server_values                          120     112      -8
    smb302_server_values                         120     112      -8
    smb21_server_values                          120     112      -8
    smb20_server_values                          120     112      -8
    smb1_server_values                           120     112      -8
    Total: Before=190218, After=190170, chg -0.03%
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- smb_common.h
+++ smb_common.h
@@ -54,6 +54,7 @@ struct dir_context;
 struct smb_version_values {
 	char		*version_string;
 	__u16		protocol_id;
+	__le16		lock_cmd;
 	__u32		req_capabilities;
 	__u32		large_lock_type;
 	__u32		exclusive_lock_type;
@@ -62,7 +63,6 @@ struct smb_version_values {
 	size_t		header_size;
 	size_t		max_header_size;
 	size_t		read_rsp_size;
-	__le16		lock_cmd;
 	unsigned int	cap_unix;
 	unsigned int	cap_nt_find;
 	unsigned int	cap_large_files;
