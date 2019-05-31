d43ed4cefd33cb38278f77f946a720e83fecd536
Hyunchul Lee <hyc.lee@gmail.com>
2019-05-31 14:29:27 +0900

    cifsd: smbd: accept clients
    When connection is requested, smbd_transport struct
    is allocated and a task is created to handle requests
    from a client.
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- transport_smbd.h
+++ transport_smbd.h
@@ -4,8 +4,27 @@
  *   Copyright (C) 2018, LG Electronics.
  */
 
-#ifndef __CIFSD_TRANSPORT_RDMA_H_
-#define __CIFSD_TRANSPORT_RDMA_H_
+#ifndef __CIFSD_TRANSPORT_SMBD_H__
+#define __CIFSD_TRANSPORT_SMBD_H__
+
+#define SMBD_PORT	5445
+
+/* SMBD negotiation response packet [MS-SMBD] 2.2.2 */
+struct smbd_negotiate_resp {
+	__le16 min_version;
+	__le16 max_version;
+	__le16 negotiated_version;
+	__le16 reserved;
+	__le16 credits_requested;
+	__le16 credits_granted;
+	__le32 status;
+	__le32 max_readwrite_size;
+	__le32 preferred_send_size;
+	__le32 max_receive_size;
+	__le32 max_fragmented_size;
+} __packed;
+
+#define SMB_DIRECT_RESPONSE_REQUESTED 0x0001
 
 #ifdef CONFIG_CIFSD_SMBDIRECT
 extern int cifsd_smbd_init(void);
@@ -15,4 +34,4 @@ int cifsd_smbd_init(void) { return 0; }
 int cifsd_smbd_destroy(void) { return 0; }
 #endif
 
-#endif
+#endif /* __CIFSD_TRANSPORT_SMBD_H__ */
