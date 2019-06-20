dd5f7b775f7d217a0bff7b699e9561d88ab80379
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2019-06-20 14:15:25 +0900

    cifsd: fix cifsd_lookup_dialect_by_id()
    cifsd_lookup_dialect_by_id() converts `dialects_count' from
    le16 to CPU native:
    
    	for (i = ARRAY_SIZE(smb_protos) - 1; i >= 0; i--) {
    		count = le16_to_cpu(dialects_count);
    		...
    	}
    
    However, smb2 passes `dialects_count' already converted from
    le16 to CPU, so we have
    
    	for (i = ARRAY_SIZE(smb_protos) - 1; i >= 0; i--) {
    		count = le16_to_cpu(le16_to_cpu(neg_req->DialectCount));
    		...
    	}
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- smb2pdu.c
+++ smb2pdu.c
@@ -6124,7 +6124,7 @@ int smb2_ioctl(struct cifsd_work *work)
 
 		neg_req = (struct validate_negotiate_info_req *)&req->Buffer[0];
 		ret = cifsd_lookup_dialect_by_id(neg_req->Dialects,
-					le16_to_cpu(neg_req->DialectCount));
+						 neg_req->DialectCount);
 		if (ret == BAD_PROT_ID || ret != conn->dialect)
 			goto out;
 
