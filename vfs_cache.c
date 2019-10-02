60fa06fc51f454b68f1b2a6d2b0dc571f0a0301c
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2019-10-02 16:50:01 +0900

    cifsd: make work ->state a single variable again
    Reduce cifsd_work struct size (on x86_64) from 224 bytes to 216.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- vfs_cache.c
+++ vfs_cache.c
@@ -372,8 +372,7 @@ static void __cifsd_close_fd(struct cifsd_file_table *ft,
 	list_for_each_entry_safe(cancel_work, ctmp, &fp->blocked_works,
 		fp_entry) {
 		list_del(&cancel_work->fp_entry);
-		cancel_work->state_closed = true;
-		cancel_work->state_cancelled = false;
+		cancel_work->state = CIFSD_WORK_CLOSED;
 		cancel_work->cancel_fn(cancel_work->cancel_argv);
 	}
 	spin_unlock(&fp->f_lock);
