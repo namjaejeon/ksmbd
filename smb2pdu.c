941500743ff65464714b81ad320a2f922698c74e
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2019-10-14 11:11:13 +0900

    cifsd: req->Channel and SMB2_CHANNEL_RDMA_V1_INVALIDATE are le32
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- smb2pdu.c
+++ smb2pdu.c
@@ -5751,7 +5751,7 @@ static ssize_t smb2_write_rdma_channel(struct cifsd_work *work,
 		return -EINVAL;
 
 	work->need_invalidate_rkey =
-		le32_to_cpu(req->Channel) == SMB2_CHANNEL_RDMA_V1_INVALIDATE;
+		(req->Channel == SMB2_CHANNEL_RDMA_V1_INVALIDATE);
 	work->remote_key = le32_to_cpu(desc->token);
 
 	data_buf = cifsd_alloc_response(length);
