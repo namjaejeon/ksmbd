namjaejeon/ksmbd

commit c670a9a0d35f43ab77a7606cea544f4d803486e4

ksmbd: verify and set signature for compound request/response
When mounting with signing(sec=ntlmv2i), The following error messages
are coming.

[ 2045.267558] CIFS: Attempting to mount //10.88.103.87/homes2
[ 2045.278424] CIFS VFS: sign fail cmd 0x3 message id 0x3
[ 2045.278429] CIFS VFS: \10.88.103.87 SMB signature verification
returned error = -13
[ 2045.278916] CIFS VFS: sign fail cmd 0x3 message id 0x4
[ 2045.278920] CIFS VFS: \10.88.103.87 SMB signature verification
returned error = -13
[ 2045.279266] CIFS VFS: sign fail cmd 0x5 message id 0x5

The problem is that cifsd doesn't set and check signature as per compound
request. This patch make cifsd correctly working signing verification and
setting with compounding and fix the file deletion issue with windows 10
client.

Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>

server.c
@@ -138,8 +138,7 @@ andx_again:
 		return TCP_HANDLER_CONTINUE;
 	}
 
-	if (work->sess && conn->ops->is_sign_req &&
-		conn->ops->is_sign_req(work, command)) {
+	if (conn->ops->is_sign_req(work, command)) {
 		ret = conn->ops->check_sign_req(work);
 		if (!ret) {
 			conn->ops->set_rsp_status(work, STATUS_ACCESS_DENIED);
@@ -226,6 +225,11 @@ static void __handle_ksmbd_work(struct ksmbd_work *work,
 				goto send;
 			}
 		}
+
+		if ((work->sess && work->sess->sign) ||
+		     smb3_11_final_sess_setup_resp(work) ||
+		     conn->ops->is_sign_req(work, command))
+			conn->ops->set_sign_rsp(work);
 	} while (is_chained_smb2_message(work));
 
 	if (work->send_no_response)
@@ -240,11 +244,7 @@ send:
 			conn->ops->set_rsp_status(work, STATUS_DATA_ERROR);
 			goto send;
 		}
-	} else if (work->sess && (work->sess->sign ||
-		smb3_final_sess_setup_resp(work) ||
-		(conn->ops->is_sign_req &&
-		conn->ops->is_sign_req(work, command))))
-		conn->ops->set_sign_rsp(work);
+	}
 
 	ksmbd_conn_write(work);
 }

smb1pdu.c
@@ -8243,16 +8243,16 @@ out:
  * smb1_is_sign_req() - handler for checking packet signing status
  * @work:	smb work containing notify command buffer
  *
- * Return:	1 if packed is signed, 0 otherwise
+ * Return:	true if packed is signed, false otherwise
  */
-int smb1_is_sign_req(struct ksmbd_work *work, unsigned int command)
+bool smb1_is_sign_req(struct ksmbd_work *work, unsigned int command)
 {
 	struct smb_hdr *rcv_hdr1 = (struct smb_hdr *)REQUEST_BUF(work);
 
 	if ((rcv_hdr1->Flags2 & SMBFLG2_SECURITY_SIGNATURE) &&
 			command != SMB_COM_SESSION_SETUP_ANDX)
-		return 1;
-	return 0;
+		return true;
+	return false;
 }
 
 /**

smb1pdu.h
@@ -1586,7 +1586,7 @@ extern int init_smb_rsp_hdr(struct ksmbd_work *work);
 extern uint16_t get_smb_cmd_val(struct ksmbd_work *work);
 extern void set_smb_rsp_status(struct ksmbd_work *work, __le32 err);
 extern int smb_allocate_rsp_buf(struct ksmbd_work *work);
-extern int smb1_is_sign_req(struct ksmbd_work *work, unsigned int command);
+extern bool smb1_is_sign_req(struct ksmbd_work *work, unsigned int command);
 extern int smb1_check_sign_req(struct ksmbd_work *work);
 extern void smb1_set_sign_rsp(struct ksmbd_work *work);
 extern int smb_check_user_session(struct ksmbd_work *work);

smb2pdu.c
@@ -7655,9 +7655,9 @@ int smb2_notify(struct ksmbd_work *work)
  * smb2_is_sign_req() - handler for checking packet signing status
  * @work:smb work containing notify command buffer
  *
- * Return:	1 if packed is signed, 0 otherwise
+ * Return:	true if packed is signed, false otherwise
  */
-int smb2_is_sign_req(struct ksmbd_work *work, unsigned int command)
+bool smb2_is_sign_req(struct ksmbd_work *work, unsigned int command)
 {
 	struct smb2_hdr *rcv_hdr2 = REQUEST_BUF(work);
 
@@ -7665,12 +7665,7 @@ int smb2_is_sign_req(struct ksmbd_work *work, unsigned int command)
 			command != SMB2_NEGOTIATE_HE &&
 			command != SMB2_SESSION_SETUP_HE &&
 			command != SMB2_OPLOCK_BREAK_HE)
-		return 1;
-
-	/* send session setup auth phase signed response */
-	if (work->sess && work->sess->sign &&
-		command == SMB2_SESSION_SETUP_HE)
-		return 1;
+		return true;
 
 	return 0;
 }
@@ -7683,23 +7678,36 @@ int smb2_is_sign_req(struct ksmbd_work *work, unsigned int command)
  */
 int smb2_check_sign_req(struct ksmbd_work *work)
 {
-	struct smb2_hdr *rcv_hdr2 = REQUEST_BUF(work);
+	struct smb2_hdr *hdr, *hdr_org;
 	char signature_req[SMB2_SIGNATURE_SIZE];
 	char signature[SMB2_HMACSHA256_SIZE];
 	struct kvec iov[1];
+	size_t len;
 
-	memcpy(signature_req, rcv_hdr2->Signature, SMB2_SIGNATURE_SIZE);
-	memset(rcv_hdr2->Signature, 0, SMB2_SIGNATURE_SIZE);
+	hdr_org = hdr = REQUEST_BUF(work);
+	if (work->next_smb2_rcv_hdr_off)
+		hdr = REQUEST_BUF_NEXT(work);
 
-	iov[0].iov_base = (char *)&rcv_hdr2->ProtocolId;
-	iov[0].iov_len = be32_to_cpu(rcv_hdr2->smb2_buf_length);
+	if (!hdr->NextCommand && !work->next_smb2_rcv_hdr_off)
+		len = be32_to_cpu(hdr_org->smb2_buf_length);
+	else if (hdr->NextCommand)
+		len = le32_to_cpu(hdr->NextCommand);
+	else
+		len = be32_to_cpu(hdr_org->smb2_buf_length) -
+			work->next_smb2_rcv_hdr_off;
+
+	memcpy(signature_req, hdr->Signature, SMB2_SIGNATURE_SIZE);
+	memset(hdr->Signature, 0, SMB2_SIGNATURE_SIZE);
+
+	iov[0].iov_base = (char *)&hdr->ProtocolId;
+	iov[0].iov_len = len;
 
 	if (ksmbd_sign_smb2_pdu(work->conn, work->sess->sess_key, iov, 1,
 		signature))
 		return 0;
 
 	if (memcmp(signature, signature_req, SMB2_SIGNATURE_SIZE)) {
-		ksmbd_debug(SMB, "bad smb2 signature\n");
+		ksmbd_err("bad smb2 signature\n");
 		return 0;
 	}
 
@@ -7713,16 +7721,36 @@ int smb2_check_sign_req(struct ksmbd_work *work)
  */
 void smb2_set_sign_rsp(struct ksmbd_work *work)
 {
-	struct smb2_hdr *rsp_hdr = RESPONSE_BUF(work);
+	struct smb2_hdr *hdr, *hdr_org;
+	struct smb2_hdr *req_hdr;
 	char signature[SMB2_HMACSHA256_SIZE];
 	struct kvec iov[2];
+	size_t len;
 	int n_vec = 1;
 
-	rsp_hdr->Flags |= SMB2_FLAGS_SIGNED;
-	memset(rsp_hdr->Signature, 0, SMB2_SIGNATURE_SIZE);
+	hdr_org = hdr = RESPONSE_BUF(work);
+	if (work->next_smb2_rsp_hdr_off)
+		hdr = RESPONSE_BUF_NEXT(work);
 
-	iov[0].iov_base = (char *)&rsp_hdr->ProtocolId;
-	iov[0].iov_len = be32_to_cpu(rsp_hdr->smb2_buf_length);
+	req_hdr = REQUEST_BUF_NEXT(work);
+
+	if (!work->next_smb2_rsp_hdr_off) {
+		len = get_rfc1002_len(hdr_org);
+		if (req_hdr->NextCommand)
+			len = ALIGN(len, 8);
+	} else {
+		len = get_rfc1002_len(hdr_org) - work->next_smb2_rsp_hdr_off;
+		len = ALIGN(len, 8);
+	}
+
+	if (req_hdr->NextCommand)
+		hdr->NextCommand = cpu_to_le32(len);
+
+	hdr->Flags |= SMB2_FLAGS_SIGNED;
+	memset(hdr->Signature, 0, SMB2_SIGNATURE_SIZE);
+
+	iov[0].iov_base = (char *)&hdr->ProtocolId;
+	iov[0].iov_len = len;
 
 	if (HAS_AUX_PAYLOAD(work)) {
 		iov[0].iov_len -= AUX_PAYLOAD_SIZE(work);
@@ -7734,7 +7762,7 @@ void smb2_set_sign_rsp(struct ksmbd_work *work)
 
 	if (!ksmbd_sign_smb2_pdu(work->conn, work->sess->sess_key, iov, n_vec,
 		signature))
-		memcpy(rsp_hdr->Signature, signature, SMB2_SIGNATURE_SIZE);
+		memcpy(hdr->Signature, signature, SMB2_SIGNATURE_SIZE);
 }
 
 /**
@@ -7791,7 +7819,7 @@ int smb3_check_sign_req(struct ksmbd_work *work)
 		return 0;
 
 	if (memcmp(signature, signature_req, SMB2_SIGNATURE_SIZE)) {
-		ksmbd_debug(SMB, "bad smb2 signature\n");
+		ksmbd_err("bad smb2 signature\n");
 		return 0;
 	}
 
@@ -8012,19 +8040,19 @@ int smb3_decrypt_req(struct ksmbd_work *work)
 	return rc;
 }
 
-int smb3_final_sess_setup_resp(struct ksmbd_work *work)
+bool smb3_11_final_sess_setup_resp(struct ksmbd_work *work)
 {
 	struct ksmbd_conn *conn = work->conn;
 	struct smb2_hdr *rsp = RESPONSE_BUF(work);
 
 	if (conn->dialect != SMB311_PROT_ID)
-		return 0;
+		return false;
 
 	if (work->next_smb2_rcv_hdr_off)
 		rsp = RESPONSE_BUF_NEXT(work);
 
 	if (le16_to_cpu(rsp->Command) == SMB2_SESSION_SETUP_HE &&
 		rsp->Status == STATUS_SUCCESS)
-		return 1;
-	return 0;
+		return true;
+	return false;
 }

smb2pdu.h
@@ -1515,7 +1515,7 @@ extern int init_smb2_neg_rsp(struct ksmbd_work *work);
 extern void smb2_set_err_rsp(struct ksmbd_work *work);
 extern int smb2_check_user_session(struct ksmbd_work *work);
 extern int smb2_get_ksmbd_tcon(struct ksmbd_work *work);
-extern int smb2_is_sign_req(struct ksmbd_work *work, unsigned int command);
+extern bool smb2_is_sign_req(struct ksmbd_work *work, unsigned int command);
 extern int smb2_check_sign_req(struct ksmbd_work *work);
 extern void smb2_set_sign_rsp(struct ksmbd_work *work);
 extern int smb3_check_sign_req(struct ksmbd_work *work);
@@ -1531,7 +1531,7 @@ extern void smb3_preauth_hash_rsp(struct ksmbd_work *work);
 extern int smb3_is_transform_hdr(void *buf);
 extern int smb3_decrypt_req(struct ksmbd_work *work);
 extern int smb3_encrypt_resp(struct ksmbd_work *work);
-extern int smb3_final_sess_setup_resp(struct ksmbd_work *work);
+extern bool smb3_11_final_sess_setup_resp(struct ksmbd_work *work);
 extern int smb2_set_rsp_credits(struct ksmbd_work *work);
 
 /* smb2 misc functions */

smb_common.h
@@ -451,7 +451,7 @@ struct smb_version_ops {
 	int (*set_rsp_credits)(struct ksmbd_work *work);
 	int (*check_user_session)(struct ksmbd_work *work);
 	int (*get_ksmbd_tcon)(struct ksmbd_work *work);
-	int (*is_sign_req)(struct ksmbd_work *work, unsigned int command);
+	bool (*is_sign_req)(struct ksmbd_work *work, unsigned int command);
 	int (*check_sign_req)(struct ksmbd_work *work);
 	void (*set_sign_rsp)(struct ksmbd_work *work);
 	int (*generate_signingkey)(struct ksmbd_session *sess);

commit e3a2d6fd17642bdc24eaead38c609e0aaeb88f13

cifsd: prevent NULL pointer dereference in signing handling
Make sure work->sess is not NULL when calling check_sign_req() to
prevent NULL pointer dereference. This was causing kernel panic when
using signing and pysmb (SMB1) as client.

Fixes: c670a9a0d35f ("ksmbd: verify and set signature for compound request/response")
Signed-off-by: Fredrik Ternerot <fredrikt@axis.com>
Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>

server.c
@@ -138,7 +138,7 @@ andx_again:
 		return TCP_HANDLER_CONTINUE;
 	}
 
-	if (conn->ops->is_sign_req(work, command)) {
+	if (work->sess && conn->ops->is_sign_req(work, command)) {
 		ret = conn->ops->check_sign_req(work);
 		if (!ret) {
 			conn->ops->set_rsp_status(work, STATUS_ACCESS_DENIED);