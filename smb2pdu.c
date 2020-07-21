icecubi/cifsd

cifsd: sign session setup response on SMB3.0 and SMB3.02
smbclient check whether session setup response is signed on SMB3.0 and
SMB3.02 like SMB3.11.

Reported-by: Hranislav Milenkovic <hmilenkovic@protonmail.com>
Reported-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>
Cc: Hranislav Milenkovic <hmilenkovic@protonmail.com>
Cc: Sebastian Gottschall <s.gottschall@dd-wrt.com>
Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>

smb2pdu.c
@@ -8079,7 +8079,7 @@ bool smb3_11_final_sess_setup_resp(struct ksmbd_work *work)
 	struct ksmbd_conn *conn = work->conn;
 	struct smb2_hdr *rsp = RESPONSE_BUF(work);
 
-	if (conn->dialect != SMB311_PROT_ID)
+	if (conn->dialect < SMB302_PROT_ID)
 		return false;
 
 	if (work->next_smb2_rcv_hdr_off)

namjaejeon/ksmbd

smb2pdu.c
@@ -8079,7 +8079,7 @@ bool smb3_11_final_sess_setup_resp(struct ksmbd_work *work)
 	struct ksmbd_conn *conn = work->conn;
 	struct smb2_hdr *rsp = RESPONSE_BUF(work);
 
-	if (conn->dialect != SMB311_PROT_ID)
+	if (conn->dialect < SMB30_PROT_ID)
 		return false;
 
 	if (work->next_smb2_rcv_hdr_off)