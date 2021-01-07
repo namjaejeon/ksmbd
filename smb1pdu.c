45e81e2a743b3928b2542614308a56a5f314722e
Namjae Jeon <namjae.jeon@samsung.com>
2021-01-07 16:17:11 +0900

    cifsd: set true to set_read_buf in smb_read_andx()
    Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>

--- smb1pdu.c
+++ smb1pdu.c
@@ -2869,9 +2869,10 @@ int smb_read_andx(struct ksmbd_work *work)
 	ksmbd_debug(SMB, "filename %s, offset %lld, count %zu\n",
 		FP_FILENAME(fp), pos, count);
 
-	if (server_conf.flags & KSMBD_GLOBAL_FLAG_CACHE_RBUF)
+	if (server_conf.flags & KSMBD_GLOBAL_FLAG_CACHE_RBUF) {
 		work->aux_payload_buf = ksmbd_find_buffer(count);
-	else
+		work->set_read_buf = true;
+	} else
 		work->aux_payload_buf = ksmbd_alloc_response(count);
 	if (!work->aux_payload_buf) {
 		err = -ENOMEM;
