fd42ea3e3a5d6ece0bb82a49a5c76c98ed852c04
Marios Makassikis <mmakassikis@freebox.fr>
2022-05-03 12:36:41 +0200

    ksmbd: smb1: remove unused parameter in find_next()
    The ResumeFileName field is decoded only to never be used. Remove
    completely.
    
    Signed-off-by: Marios Makassikis <mmakassikis@freebox.fr>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- smb1pdu.c
+++ smb1pdu.c
@@ -6211,7 +6211,6 @@ static int find_next(struct ksmbd_work *work)
 	int data_alignment_offset = 0;
 	int rc = 0, reclen = 0;
 	__u16 sid;
-	char *name = NULL;
 	char *pathname = NULL;
 	int header_size, srch_cnt, struct_sz;
 
@@ -6220,16 +6219,6 @@ static int find_next(struct ksmbd_work *work)
 	req_params = (struct smb_com_trans2_fnext_req_params *)
 		(work->request_buf + le16_to_cpu(req->ParameterOffset) + 4);
 	sid = req_params->SearchHandle;
-
-	/*Currently no usage of ResumeFilename*/
-	name = req_params->ResumeFileName;
-	name = smb_strndup_from_utf16(name, NAME_MAX, 1, conn->local_nls);
-	if (IS_ERR(name)) {
-		rsp->hdr.Status.CifsError = STATUS_NO_MEMORY;
-		return PTR_ERR(name);
-	}
-	ksmbd_debug(SMB, "FileName after unicode conversion %s\n", name);
-	kfree(name);
 
 	dir_fp = ksmbd_lookup_fd_fast(work, sid);
 	if (!dir_fp) {
