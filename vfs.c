7319fc3869491265cc9317981a448c69f7f5fb56
Amir Goldstein <amir73il@gmail.com>
2022-08-30 16:12:41 +0900

    ksmbd: vfs: fix copy_file_range() regression in cross-fs copies
    A regression has been reported by Nicolas Boichat, found while using the
    copy_file_range syscall to copy a tracefs file.
    
    Before commit 5dae222a5ff0 ("vfs: allow copy_file_range to copy across
    devices") the kernel would return -EXDEV to userspace when trying to
    copy a file across different filesystems.  After this commit, the
    syscall doesn't fail anymore and instead returns zero (zero bytes
    copied), as this file's content is generated on-the-fly and thus reports
    a size of zero.
    
    Another regression has been reported by He Zhe - the assertion of
    WARN_ON_ONCE(ret == -EOPNOTSUPP) can be triggered from userspace when
    copying from a sysfs file whose read operation may return -EOPNOTSUPP.
    
    Since we do not have test coverage for copy_file_range() between any two
    types of filesystems, the best way to avoid these sort of issues in the
    future is for the kernel to be more picky about filesystems that are
    allowed to do copy_file_range().
    
    This patch restores some cross-filesystem copy restrictions that existed
    prior to commit 5dae222a5ff0 ("vfs: allow copy_file_range to copy across
    devices"), namely, cross-sb copy is not allowed for filesystems that do
    not implement ->copy_file_range().
    
    Filesystems that do implement ->copy_file_range() have full control of
    the result - if this method returns an error, the error is returned to
    the user.  Before this change this was only true for fs that did not
    implement the ->remap_file_range() operation (i.e.  nfsv3).
    
    Filesystems that do not implement ->copy_file_range() still fall-back to
    the generic_copy_file_range() implementation when the copy is within the
    same sb.  This helps the kernel can maintain a more consistent story
    about which filesystems support copy_file_range().
    
    nfsd and ksmbd servers are modified to fall-back to the
    generic_copy_file_range() implementation in case vfs_copy_file_range()
    fails with -EOPNOTSUPP or -EXDEV, which preserves behavior of
    server-side-copy.
    
    fall-back to generic_copy_file_range() is not implemented for the smb
    operation FSCTL_DUPLICATE_EXTENTS_TO_FILE, which is arguably a correct
    change of behavior.
    
    Fixes: 5dae222a5ff0 ("vfs: allow copy_file_range to copy across devices")
    Link: https://lore.kernel.org/linux-fsdevel/20210212044405.4120619-1-drinkcat@chromium.org/
    Link: https://lore.kernel.org/linux-fsdevel/CANMq1KDZuxir2LM5jOTm0xx+BnvW=ZmpsG47CyHFJwnw7zSX6Q@mail.gmail.com/
    Link: https://lore.kernel.org/linux-fsdevel/20210126135012.1.If45b7cdc3ff707bc1efa17f5366057d60603c45f@changeid/
    Link: https://lore.kernel.org/linux-fsdevel/20210630161320.29006-1-lhenriques@suse.de/
    Reported-by: Nicolas Boichat <drinkcat@chromium.org>
    Reported-by: kernel test robot <oliver.sang@intel.com>
    Signed-off-by: Luis Henriques <lhenriques@suse.de>
    Fixes: 64bf5ff58dff ("vfs: no fallback for ->copy_file_range")
    Link: https://lore.kernel.org/linux-fsdevel/20f17f64-88cb-4e80-07c1-85cb96c83619@windriver.com/
    Reported-by: He Zhe <zhe.he@windriver.com>
    Tested-by: Namjae Jeon <linkinjeon@kernel.org>
    Tested-by: Luis Henriques <lhenriques@suse.de>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- vfs.c
+++ vfs.c
@@ -2415,6 +2415,12 @@ int ksmbd_vfs_copy_file_ranges(struct ksmbd_work *work,
 
 		ret = vfs_copy_file_range(src_fp->filp, src_off,
 					  dst_fp->filp, dst_off, len, 0);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0)
+		if (ret == -EOPNOTSUPP || ret == -EXDEV)
+			ret = generic_copy_file_range(src_fp->filp, src_off,
+						      dst_fp->filp, dst_off,
+						      len, 0);
+#endif
 		if (ret < 0)
 			return ret;
 
