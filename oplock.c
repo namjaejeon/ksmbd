820d82db86364c0c1e9fd3badddc24c6a39a9a86
Namjae Jeon <linkinjeon@kernel.org>
2025-03-24 23:43:11 +0900

    ksmbd: fix r_count dec/increment mismatch
    r_count is only increased when there is an oplock break wait,
    so r_count inc/decrement are not paired. This can cause r_count
    to become negative, which can lead to a problem where the ksmbd
    thread does not terminate.
    
    Fixes: 3aa660c05924 ("ksmbd: prevent connection release during oplock break notification")
    Reported-by: Norbert Szetei <norbert@doyensec.com>
    Tested-by: Norbert Szetei <norbert@doyensec.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- oplock.c
+++ oplock.c
@@ -809,8 +809,8 @@ static int smb1_oplock_break_noti(struct oplock_info *opinfo)
 	work->request_buf = (char *)opinfo;
 	work->conn = conn;
 
+	ksmbd_conn_r_count_inc(conn);
 	if (opinfo->op_state == OPLOCK_ACK_WAIT) {
-		ksmbd_conn_r_count_inc(conn);
 		INIT_WORK(&work->work, __smb1_oplock_break_noti);
 		ksmbd_queue_work(work);
 
@@ -928,8 +928,8 @@ static int smb2_oplock_break_noti(struct oplock_info *opinfo)
 	work->conn = conn;
 	work->sess = opinfo->sess;
 
+	ksmbd_conn_r_count_inc(conn);
 	if (opinfo->op_state == OPLOCK_ACK_WAIT) {
-		ksmbd_conn_r_count_inc(conn);
 		INIT_WORK(&work->work, __smb2_oplock_break_noti);
 		ksmbd_queue_work(work);
 
@@ -1037,8 +1037,8 @@ static int smb2_lease_break_noti(struct oplock_info *opinfo)
 	work->conn = conn;
 	work->sess = opinfo->sess;
 
+	ksmbd_conn_r_count_inc(conn);
 	if (opinfo->op_state == OPLOCK_ACK_WAIT) {
-		ksmbd_conn_r_count_inc(conn);
 		INIT_WORK(&work->work, __smb2_lease_break_noti);
 		ksmbd_queue_work(work);
 		wait_for_break_ack(opinfo);
