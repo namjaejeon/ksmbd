372c7b54dbd08c160312144495aacfe882d31bb4
Namjae Jeon <linkinjeon@kernel.org>
2025-03-27 09:28:26 +0900

    ksmbd: fix session use-after-free in multichannel connection
    There is a race condition between session setup and
    ksmbd_sessions_deregister. The session can be freed before the connection
    is added to channel list of session.
    This patch check reference count of session before freeing it.
    
    Cc: stable@vger.kernel.org
    Reported-by: Sean Heelan <seanheelan@gmail.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- mgmt/user_session.c
+++ mgmt/user_session.c
@@ -181,7 +181,7 @@ static void ksmbd_expire_session(struct ksmbd_conn *conn)
 	down_write(&sessions_table_lock);
 	down_write(&conn->session_lock);
 	xa_for_each(&conn->sessions, id, sess) {
-		if (atomic_read(&sess->refcnt) == 0 &&
+		if (atomic_read(&sess->refcnt) <= 1 &&
 		    (sess->state != SMB2_SESSION_VALID ||
 		     time_after(jiffies,
 			       sess->last_active + SMB2_SESSION_TIMEOUT))) {
@@ -235,15 +235,16 @@ void ksmbd_sessions_deregister(struct ksmbd_conn *conn)
 			if (!ksmbd_chann_del(conn, sess) &&
 			    xa_empty(&sess->ksmbd_chann_list)) {
 #ifdef CONFIG_SMB_INSECURE_SERVER
-			if (hash_hashed(&sess->hlist))
-				hash_del(&sess->hlist);
+				if (hash_hashed(&sess->hlist))
+					hash_del(&sess->hlist);
 #else
 				hash_del(&sess->hlist);
 #endif
 				down_write(&conn->session_lock);
 				xa_erase(&conn->sessions, sess->id);
 				up_write(&conn->session_lock);
-				ksmbd_session_destroy(sess);
+				if (atomic_dec_and_test(&sess->refcnt))
+					ksmbd_session_destroy(sess);
 			}
 		}
 	}
@@ -267,7 +268,8 @@ void ksmbd_sessions_deregister(struct ksmbd_conn *conn)
 #else
 			hash_del(&sess->hlist);
 #endif
-			ksmbd_session_destroy(sess);
+			if (atomic_dec_and_test(&sess->refcnt))
+				ksmbd_session_destroy(sess);
 		}
 	}
 	up_write(&conn->session_lock);
@@ -343,8 +345,8 @@ void ksmbd_user_session_put(struct ksmbd_session *sess)
 
 	if (atomic_read(&sess->refcnt) <= 0)
 		WARN_ON(1);
-	else
-		atomic_dec(&sess->refcnt);
+	else if (atomic_dec_and_test(&sess->refcnt))
+		ksmbd_session_destroy(sess);
 }
 
 struct preauth_session *ksmbd_preauth_session_alloc(struct ksmbd_conn *conn,
@@ -460,7 +462,7 @@ static struct ksmbd_session *__session_create(int protocol)
 	xa_init(&sess->rpc_handle_list);
 	sess->sequence_number = 1;
 	rwlock_init(&sess->tree_conns_lock);
-	atomic_set(&sess->refcnt, 1);
+	atomic_set(&sess->refcnt, 2);
 
 	switch (protocol) {
 #ifdef CONFIG_SMB_INSECURE_SERVER
