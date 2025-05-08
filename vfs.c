63bd67769512d4addce6261df11c777a50730752
Namjae Jeon <linkinjeon@kernel.org>
2025-05-08 16:42:24 +0900

    ksmbd: fix stream write failure
    If there is no stream data in file, v_len is zero.
    So, If position(*pos) is zero, stream write will fail
    due to stream write position validation check.
    This patch reorganize stream write position validation.
    
    Fixes: 0ca6df4f40cf ("ksmbd: prevent out-of-bounds stream writes by validating *pos")
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- vfs.c
+++ vfs.c
@@ -689,10 +689,15 @@ static int ksmbd_vfs_stream_write(struct ksmbd_file *fp, char *buf, loff_t *pos,
 	ksmbd_debug(VFS, "write stream data pos : %llu, count : %zd\n",
 		    *pos, count);
 
+	if (XATTR_SIZE_MAX <= *pos) {
+		pr_err("stream write position %lld is out of bounds\n",	*pos);
+		return -EINVAL;
+	}
+
 	size = *pos + count;
 	if (size > XATTR_SIZE_MAX) {
 		size = XATTR_SIZE_MAX;
-		count = (*pos + count) - XATTR_SIZE_MAX;
+		count = XATTR_SIZE_MAX - *pos;
 	}
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)
@@ -707,13 +712,6 @@ static int ksmbd_vfs_stream_write(struct ksmbd_file *fp, char *buf, loff_t *pos,
 	if (v_len < 0) {
 		pr_err("not found stream in xattr : %zd\n", v_len);
 		err = v_len;
-		goto out;
-	}
-
-	if (v_len <= *pos) {
-		pr_err("stream write position %lld is out of bounds (stream length: %zd)\n",
-				*pos, v_len);
-		err = -EINVAL;
 		goto out;
 	}
 
