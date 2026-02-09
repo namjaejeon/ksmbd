3b920a51f7708dbc097aa869905fe887b51a0add
Namjae Jeon <linkinjeon@kernel.org>
2026-02-09 10:27:26 +0900

    ksmbd: add chann_lock to protect ksmbd_chann_list xarray
    ksmbd_chann_list xarray lacks synchronization, allowing use-after-free in
    multi-channel sessions (between lookup_chann_list() and ksmbd_chann_del).
    
    Adds rw_semaphore chann_lock to struct ksmbd_session and protects
    all xa_load/xa_store/xa_erase accesses.
    
    Cc: stable@vger.kernel.org
    Reported-by: Igor Stepansky <igor.stepansky@orca.security>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- smb2pdu.c
+++ smb2pdu.c
@@ -85,7 +85,13 @@ static inline bool check_session_id(struct ksmbd_conn *conn, u64 id)
 
 struct channel *lookup_chann_list(struct ksmbd_session *sess, struct ksmbd_conn *conn)
 {
-	return xa_load(&sess->ksmbd_chann_list, (long)conn);
+	struct channel *chann;
+
+	down_read(&sess->chann_lock);
+	chann = xa_load(&sess->ksmbd_chann_list, (long)conn);
+	up_read(&sess->chann_lock);
+
+	return chann;
 }
 
 /**
@@ -1577,8 +1583,10 @@ binding_session:
 				return -ENOMEM;
 
 			chann->conn = conn;
+			down_write(&sess->chann_lock);
 			old = xa_store(&sess->ksmbd_chann_list, (long)conn, chann,
 					KSMBD_DEFAULT_GFP);
+			up_write(&sess->chann_lock);
 			if (xa_is_err(old)) {
 				kfree(chann);
 				return xa_err(old);
@@ -1676,8 +1684,10 @@ binding_session:
 				return -ENOMEM;
 
 			chann->conn = conn;
+			down_write(&sess->chann_lock);
 			old = xa_store(&sess->ksmbd_chann_list, (long)conn,
 					chann, KSMBD_DEFAULT_GFP);
+			up_write(&sess->chann_lock);
 			if (xa_is_err(old)) {
 				kfree(chann);
 				return xa_err(old);
